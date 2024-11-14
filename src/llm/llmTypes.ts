6791b322dec6b752a3cda6afbfba43924482c3c7
sdilnaz <dilnaz.sembekova@gmail.com>
2024-11-14 22:58:11 +0500

    changes llTypes

--- src/llm/llmTypes.ts
+++ src/llm/llmTypes.ts
@@ -8,10 +8,7 @@ export const quizTemplates = {
       type: "multipleChoiceVideo",
       description: "",  // word
       options: [],      // List of answer options.
-      correctAnswer: {
-        word: "",
-        videoUrl: "",
-      },
+      correctAnswer: "", // video url of  correct word
       xpValue: 10,      // XP points for the question.
     },
     multipleChoiceText: {
@@ -28,39 +25,36 @@ export const quizTemplates = {
       correctAnswer: true, // Boolean for the correct answer.
       xpValue: 5,
     },
-    // matching: {
-    //   type: "matching",
-    //   words:[],
-    //   videoUrls:[],
-    //   description: "",  // Instructions for the matching task.
-    //   pairs: [  // Array of correct word/video matches.
-    //     {
-    //         word: "",
-    //         videoUrl: "",
-    //     },
-    //     {
-    //         word: "",
-    //         videoUrl: "",
-    //     },
-    //     {
-    //         word: "",
-    //         videoUrl: "",
-    //     },
-    //     {
-    //         word: "",
-    //         videoUrl: "",
-    //     },
-    //   ],      
-    //   xpValue: 15,
-    // },
+    matching: {
+      type: "matching",
+      words:[],
+      videoUrls:[],
+      description: "",  // Instructions for the matching task.
+      pairs: [  // Array of correct word/video matches.
+        {
+            word: "",
+            videoUrl: "",
+        },
+        {
+            word: "",
+            videoUrl: "",
+        },
+        {
+            word: "",
+            videoUrl: "",
+        },
+        {
+            word: "",
+            videoUrl: "",
+        },
+      ],      
+      xpValue: 15,
+    },
     fillInBlank: {
       type: "fillInBlank",
       videoUrl: "",
       description: "",  // Sentence with a blank (e.g., "Hello, [____].")
-      correctAnswer: {
-        word: "",
-        videoUrl: "",
-      }, // Correct word to fill in the blank.
+      correctAnswer: "",// Correct word to fill in the blank.
       xpValue: 10,
     }
   };
